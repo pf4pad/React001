4883f422cef2346a141ef5f7e188434033b44d94
pf4pad <65763670+pf4pad@users.noreply.github.com>
2025-09-17 23:30:20 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { Layout } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+const contentStyle = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#0958d9",
+};
+const siderStyle = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#1677ff",
+};
+const footerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#4096ff",
+};
+
+export default function App() {
+  <Layout>
+    <Header style={headerStyle}>Header</Header>
+    <Layout>
+      <Sider width="25%" style={siderStyle}>
+        Sider
+      </Sider>
+      <Content style={contentStyle}>Content</Content>
+    </Layout>
+    <Footer style={footerStyle}>Footer</Footer>
+  </Layout>;
+}
